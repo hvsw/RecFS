1411768925cd7cdb1ea3433358c9c4c6d86e2327
Unknown <llpassos@inf.ufrgs.br>
2019-06-20 18:30:00 -0300

    Reuse
    Função para criar um novo handle e para criar um caminho absoluto a partir do atual

---
+++ useful.c
@@ -0,0 +1,61 @@
+int makeAnewHandle(){
+    int i;
+
+    for(i = 0; i < MAX_NUM_FILES; i++){
+        if(openFiles[i].file == -1){
+            return (i+1);
+        }
+    }
+    //se chegou até aqui é pq n encontrou nenhuma posição no array de 10 para botar um novo arquivo
+    return -1;
+}
+
+int toAbsolutePath(char * path, char * currPath, char ** output) {
+    int i;
+    int numTokens;
+    char ** tokenizedPath;
+    char * cutToken;
+    int bufferSize = (strlen(path) + 1 + strlen(currPath) + 1);
+    char * buffer = malloc(sizeof(char)*bufferSize);
+    char * pathcpy = malloc(sizeof(char)*(strlen(path) + 1));
+
+    strcpy(pathcpy,path);
+
+
+    if(pathcpy[0] == '/'){
+        buffer[0] = '\0';
+    } else {
+        strcpy(buffer,currPath);
+    }
+
+    numTokens = tokenizePath(pathcpy, &tokenizedPath);
+
+    for(i = 0; i < numTokens; i++) {
+        if (strcmp(tokenizedPath[i],"..") == 0) {
+            if(strcmp(buffer,"/") != 0){
+                cutToken = strrchr(buffer, '/');
+                *cutToken = '\0';
+            }
+            if(strcmp(buffer,"") == 0) {
+                strcpy(buffer,"/");
+            }
+        } else{
+            if (strcmp(tokenizedPath[i],".") != 0) {
+                if(strcmp(buffer,"/") != 0){
+                    strcat(buffer,"/");
+                }
+                strcat(buffer,tokenizedPath[i]);
+            }
+        }
+    }
+
+    *output = malloc(sizeof(char)*(strlen(buffer)+ 1));
+
+    strcpy(*output, buffer);
+
+    free(buffer);
+    free(pathcpy);
+
+    return 0;
+
+}
